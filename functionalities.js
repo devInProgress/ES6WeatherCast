5ee668eadb1050f0709895175179c0952f0135bb
Naved <naved1234@gmail.com>
2018-08-30 13:59:49 +0530

    some changes

--- functionalities.js
+++ functionalities.js
@@ -1,9 +1,19 @@
 import * as ELEMENTS from './elements.js';
+import { Http } from "./http.js";
+
 export let functions = {
-  searchWeather: () => {
+  searchWeather: async () => {
     const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
     if (CITY_NAME.length == 0) {
       return alert('Please enter a city name');
+    }
+    const APIKEY = '0773d19c21418bec6a46b58f59a38d89';
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APIKEY;
+    try {
+      const responseData = await Http.fetchData(url);
+    }
+    catch (error) {
+      return console.log(error);
     }
   }
 };
---
+++ weather-data.js
@@ -0,0 +1,17 @@
+export class WeatherData {
+  constructor(cityName, description) {
+    this.cityName = cityName;
+    this.description = description;
+    this.temperature = '';
+  }
+}
+
+export const WEATHER_PROXY_HANDLER = {
+  get: (target, property) => {
+    return Reflect.get(target, property);
+  },
+  set: (target, property, value) => {
+    const newValue = value.toFixed(2) + 'C.';
+    return Reflect.set(target, property, newValue);
+  }
+};
