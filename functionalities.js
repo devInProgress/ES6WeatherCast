8cfd0d878eed9b7f26247aa961d845c38e9c62bb
Naved <naved1234@gmail.com>
2018-08-30 12:30:20 +0530

    some changes

--- functionalities.js
+++ functionalities.js
@@ -1,6 +1,10 @@
-export const functions = {
-  searchWeather: function () {
-    alert('clicked');
+import * as ELEMENTS from './elements.js';
+export let functions = {
+  searchWeather: () => {
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+    if (CITY_NAME.length == 0) {
+      return alert('Please enter a city name');
+    }
   }
 };
 
