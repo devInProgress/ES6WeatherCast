01a06d4c74993ddc41f682b06bb0d7bcf8ee2c7c
Naved <naved1234@gmail.com>
2018-08-30 12:17:42 +0530

    some changes

---
+++ app.js
@@ -0,0 +1,4 @@
+import * as ELEMENTS from 'elements.js';
+import { functions } from 'functionalities.js';
+ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', functions.searchWeather);
+
---
+++ functionalities.js
@@ -0,0 +1,6 @@
+export const functions = {
+  searchWeather: function () {
+    alert('clicked');
+  }
+};
+
